59ae9e3a4fe6d27403294a0b98b1933e608f7fc3
Benedikt <benedikt@Benedikts-MacBook-Pro.local>
2023-05-31 14:49:26 +0200

    Auditing Listener Task

--- src/main/java/com/xbaychallenge/listing/repository/ListingEntity.java
+++ src/main/java/com/xbaychallenge/listing/repository/ListingEntity.java
@@ -2,16 +2,24 @@ package com.xbaychallenge.listing.repository;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import java.time.Instant;
 import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "listings")
 public class ListingEntity {
 
@@ -28,4 +36,7 @@ public class ListingEntity {
     private double price;
 
     private UUID seller;
+
+    @CreatedDate
+    private Instant createdDate;
 }
